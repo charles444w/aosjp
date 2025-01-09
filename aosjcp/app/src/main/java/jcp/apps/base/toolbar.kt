1e3c63b28b67243f76a148aed20e1e9e3611aa70
DEVELOPMENT\charles.wong <charles.wong@aastocks.com>
2025-01-09 12:57:45 +0800

    update

--- aosjcp/app/src/main/java/jcp/apps/base/toolbar.kt
+++ aosjcp/app/src/main/java/jcp/apps/base/toolbar.kt
@@ -3,10 +3,16 @@ package jcp.apps.base
 import android.widget.Toolbar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -23,25 +29,31 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun AppToolBar() {
-    Row(
-        modifier = Modifier
-            .height(41.dp)
-            .fillMaxWidth()
-            .background(color = Color.Green)
-            .padding(5.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
-        Text(
+    Box(modifier = Modifier
+        .height(41.dp)
+        .fillMaxWidth()
+        .background(color = Color.Green)
+        .padding(5.dp)) {
+        Row(
             modifier = Modifier
-                .weight(1f)
-                .background(Color.LightGray),
+                .height(41.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(modifier = Modifier.size(25.dp, 25.dp),imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+            Spacer(modifier = Modifier.weight(1f))
+            Icon(modifier = Modifier.size(25.dp, 25.dp),imageVector = Icons.Filled.Home, contentDescription = null)
+
+        }
+       Text(
+            modifier = Modifier.fillMaxSize().padding(start = 30.dp, end = 30.dp)
+                .background(Color.LightGray).wrapContentHeight(align = Alignment.CenterVertically),
+
             textAlign = TextAlign.Center,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis,
             text = "Home Page"
         )
-        Icon(imageVector = Icons.Filled.Home, contentDescription = null)
-
     }
+
 }
