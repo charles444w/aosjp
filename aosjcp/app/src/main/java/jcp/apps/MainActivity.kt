69ea7324aa0f38bc1bfdfe356a43eaf10f24b124
DEVELOPMENT\charles.wong <charles.wong@aastocks.com>
2025-01-16 13:19:44 +0800

    update 5

--- aosjcp/app/src/main/java/jcp/apps/MainActivity.kt
+++ aosjcp/app/src/main/java/jcp/apps/MainActivity.kt
@@ -108,7 +108,7 @@ class MainActivity : ComponentActivity() {
 
             AosjcpTheme {
 
-                //main 4
+                //main 5
                 MainScreen()
                 //  val dataStore: DataStore<Preferences> = .createDataStore(name = "my_preferences")
               //  MainScreen()
